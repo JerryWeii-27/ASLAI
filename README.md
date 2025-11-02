fa2e8205d2a3584e22dbf13202c5ad566001f414
Jerry Wei <79512937+JerryWeii-27@users.noreply.github.com>
2025-11-01 22:43:49 -0400

    ver 2

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ASLAI
 
+Dataset link: https://data.mendeley.com/datasets/48dg9vhmyk/2
+
+**Thigns to note:**
+- Rotated all 'Z' images so that the finger points up. The original orientation in the dataset is wrong.
+- Moreover, 'Z' is suppose to be a dynamic sign. The signer should move their finger in a 'Z' shape. However, image classification model cannot recognize that. Therefore, it is currently indistinguishable from 'G'.
+- If all 'G' images are taken from another angle, with the hole formed by thumb and middle finger facing the camera, the model would be able to tell 'G' form 'Z'. 
+- 'J' in this dataset is wrong, but if you do the wrong sign, it would be recognized.
+- 'J' is also a dynamic sign, maybe that is why it is wrong.
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import shutil
 import random
 import time
-import torch
+from PIL import Image
 from concurrent.futures import ThreadPoolExecutor
+import torch
 from ultralytics import YOLO
 
 
@@ -13,19 +14,31 @@ def copy_images(img_list, src_dir, dst_dir):
         shutil.copy(os.path.join(src_dir, img), os.path.join(dst_dir, img))
 
 
+def convert_to_grayscale(src_dir):
+    for root, _, files in os.walk(src_dir):
+        for f in files:
+            if f.lower().endswith((".jpg", ".png")):
+                path = os.path.join(root, f)
+                img = Image.open(path).convert("L")  # L = grayscale
+                img.save(path)
+
+
 def setup_data():
     """
     This prepares training data for YOLO.
     """
 
-    start_time = time.perf_counter()
     SOURCE_DIR = "asl_alphabet_train"
     TRAIN_DIR = "train"
     VAL_DIR = "val"
 
+    if os.path.exists(TRAIN_DIR) or os.path.exists(VAL_DIR):
+        print(f"Delete {TRAIN_DIR} and {VAL_DIR} first.")
+        return
+
     os.makedirs(TRAIN_DIR, exist_ok=True)
     os.makedirs(VAL_DIR, exist_ok=True)
-
+    start_time = time.perf_counter()
     for label in os.listdir(SOURCE_DIR):
         print(label)
         letter_dir = os.path.join(SOURCE_DIR, label)
@@ -37,9 +50,10 @@ def setup_data():
             f for f in os.listdir(letter_dir) if f.lower().endswith((".jpg"))
         ]  # I think my dataset if pure jpg.
 
-        # Shuffle the list for whatever reason. ChatGPT says it will make training less biased.
-        random.shuffle(images)
+        # Shuffle the list.
 
+        random.shuffle(images)
+        images = images[: int(len(images) * 1)]
         # Split the images into 80% for training, 20% to validate training.
         split = int(0.8 * len(images))
         train_imgs = images[:split]
@@ -48,22 +62,30 @@ def setup_data():
         os.makedirs(os.path.join(TRAIN_DIR, label), exist_ok=True)
         os.makedirs(os.path.join(VAL_DIR, label), exist_ok=True)
 
-        max_workers = 8
+        max_workers = 16
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = []
             chunk_size = len(train_imgs) // max_workers
             dst_dir = os.path.join(TRAIN_DIR, label)
             for i in range(0, len(train_imgs), chunk_size):
-
                 cur_list = train_imgs[i : i + chunk_size]
-                executor.submit(copy_images, cur_list, letter_dir, dst_dir)
+                futures.append(
+                    executor.submit(copy_images, cur_list, letter_dir, dst_dir)
+                )
+            for f in futures:
+                f.result()
 
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = []
             chunk_size = len(val_imgs) // max_workers
             dst_dir = os.path.join(VAL_DIR, label)
             for i in range(0, len(val_imgs), chunk_size):
-                cur_list = train_imgs[i : i + chunk_size]
-                executor.submit(copy_images, cur_list, letter_dir, dst_dir)
-
+                cur_list = val_imgs[i : i + chunk_size]
+                futures.append(
+                    executor.submit(copy_images, cur_list, letter_dir, dst_dir)
+                )
+            for f in futures:
+                f.result()
     end_time = time.perf_counter()
     delta_time = end_time - start_time
     print(f"Execution time: {delta_time:.2f} seconds")
@@ -71,22 +93,68 @@ def setup_data():
 
 def train_yolo():
     """
-    Use YOLO to train the classification model.
+    Trains a YOLOv11n classification model on ASL data with improved regularization
+    and training stability to reduce overfitting.
     """
+
+    torch.backends.cudnn.benchmark = True
+
     model = YOLO("yolov8n-cls.pt")
+
     model.train(
-        data="",
-        epochs=30,  # Number of training cycles.
-        imgsz=224,  # Image size. I used 200 here because our dataset is 200x200.
-        batch=256,  # Number of images processes at once.
-        lr0=0.001,  # Starting learning rate, basically means how fast weights change.
-        augment=True,  # Enable random changes, like flips, rotations, and lighting to data.
-        patience=5,  # Stop early if validation accuracy did not improve after 10 epochs.
+        data="./",
+        epochs=12,
+        batch=64,
+        imgsz=320,
+        augment=True,  # Enable augmentations
+        # Augmentation settings
+        flipud=0.0,
+        fliplr=0.5,  # Horizontal flips
+        hsv_h=0.05,  # 5% hue variation
+        hsv_s=0.7,  # Saturation
+        hsv_v=0.5,  # Brightness
+        degrees=20.0,  # Rotation
+        translate=0.1,  # Position
+        scale=0.2,  # 20% random zoom
+        shear=5.0,  # Shear
+        cutmix=0.1,
+
+        patience=5,  # Early stopping
+        amp=True,  # Mixed precision for speed
+        optimizer="AdamW",
+        lr0=5e-4,  # Lower LR, smoother convergence
+        weight_decay=1e-3,  # Stronger regularization for overfaitting control
+        label_smoothing=0.1,  # Softens overconfident predictions
     )
+
+    print(model.names)
+    # test_yolo()
+
+
+def resume_yolo():
+    """
+    Resume a interrupted YOLO training.
+    """
+    torch.backends.cudnn.benchmark = True
+    BASE_DIR = "runs/classify"
+    try:
+        dirs = [
+            int(d[5:] or 1)
+            for d in os.listdir(BASE_DIR)
+            if os.path.isdir(os.path.join(BASE_DIR, d))
+        ]
+        last_path = os.path.join(
+            BASE_DIR, "train" + str(sorted(dirs)[-1]), "weights", "last.pt"
+        )
+    except Exception as e:
+        print(e)
+        return
+    model = YOLO(last_path)
+    # model.train(resume=True)
     metrics = model.val()
     print("\nValidation Results:")
     print(metrics)
-    model.export(format="tflite")
+
 
 def main():
     """
@@ -98,6 +166,7 @@ def main():
     if torch.cuda.is_available():
         print("GPU:", torch.cuda.get_device_name(0))
     train_yolo()
+    # resume_yolo()
 
 
 if __name__ == "__main__":
